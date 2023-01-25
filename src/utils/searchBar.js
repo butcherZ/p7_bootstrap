5881ee7b5f27d03f6231d976b6f1b2594b82b5d3
ziyi <daydreamnation@live.com>
2023-01-25 18:05:52 +0100

    feat(): add algo2 for searchbar

--- src/utils/searchBar.js
+++ src/utils/searchBar.js
@@ -1,18 +1,35 @@
 const filteredRecipes = (recipes, searchBar) => {
+  console.log(recipes);
   searchBar.addEventListener("keyup", (e) => {
     if (e.target.value.length >= 3) {
       recipesSection.innerHTML = "";
       const query = e.target.value.toLowerCase();
-      const results = recipes.filter((recipe) => {
-        return (
-          recipe.name.toLowerCase().startsWith(query) ||
-          recipe.description.toLowerCase().includes(query) ||
-          recipe.ingredients.some((ingredients) =>
-            ingredients.ingredients.toLowerCase().includes(query)
-          )
-        );
-      });
-      console.log("results are", results);
+
+      console.log("query is ", query);
+      const results = [];
+
+      for (let i = 0; i < recipes.length; i++) {
+        const { name, ingredients, description } = recipes[i];
+        const includesInName = name.toLowerCase().includes(query.toLowerCase());
+        const includesInDescription = description
+          .toLowerCase()
+          .includes(query.toLowerCase());
+        let includesInIngredients = false;
+        for (let j = 0; j < ingredients.length; j++) {
+          console.log("ingredients", ingredients);
+          if (
+            ingredients[j].ingredients
+              .toLowerCase()
+              .includes(query.toLowerCase())
+          ) {
+            includesInIngredients = true;
+          }
+        }
+        if (includesInName || includesInDescription || includesInIngredients) {
+          results.push(recipes[i]);
+        }
+      }
+      // console.log("results are", results);
       createRecipeCards(results);
       listenOnInputs(results);
       if (!results.length) {
